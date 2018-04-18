85e5a8cfb3a0ba799c47bbe98be123e3336cfb90
Warren Jasper <wjasper@ncsu.edu>
2018-04-17 22:29:38 -0400

    button_timer.py

--- Python/button_timer.py
+++ Python/button_timer.py
@@ -1,87 +1,133 @@
 #! /usr/bin/python3
 
+import random
 import RPi.GPIO as GPIO
 import time
-import random
-from timeit import default_timer as timer
-import datetime
+import os
 
-def Blink(n, delay=0.25):
-    for i in range(n):
-        GPIO.output(3, True)  # Turn LED ON
-        time.sleep(delay)
-        GPIO.output(3, False)  # Turn LED OFF
-        time.sleep(delay)
+#constants used for testing reflexes
+lightOn = 1
+lightOff = 2
+BuzzerOn = 3
+BuzzerOff = 4
 
-def callback(channel):
-    global end
-    end = timer()
-    GPIO.remove_event_detect(channel)
-
-GPIO.setwarnings(False)
+piNum = 14 #The number of the Pi being used in the lab
+LED = 19
+button = 26
+buzzer = 21
+user = ""
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(3, GPIO.OUT)  # LED
-GPIO.output(3, False)
+GPIO.setup(LED, GPIO.OUT)
+GPIO.setup(buzzer, GPIO.OUT)
+GPIO.setup(button, GPIO.IN, GPIO.PUD_DOWN)
+tests = [1,1,1,2,2,2,3,3,3,4,4,4]
 
-def timeStamped(fname, fmt='%Y-%m-%d-%H-%M-%S_{fname}'):
-    return datetime.datetime.now().strftime(fmt).format(fname=fname)
-file_name = timeStamped('Reaction.txt')
-file = open(file_name, 'w')
-file.write('gender' + '	' + 'reaction' + '\n')
-Reaction = []
-Gender = []
-nStudent = int(input('Please enter the number of participants: '))
+def determineFilename(user):
+    directory = "./results"
+    target = "record" + str(piNum) + user + "_"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
-for x in range(0, nStudent):
-    gender = input('Please choose your gender: 1 for Female   2 for Male   3 for Not applicable' + '\n')
-    if gender == '1':
-        gender = 'Female'
-    elif gender == '2':
-        gender = 'Male'
-    elif gender == '3':
-        gender = 'Not applicable'
-    Gender.append(gender)
-    print('Press the button as soon as possible when you see the LED is on. Game \
-          will start after the LED blinks three times.' + '\n')
+    listFiles = os.listdir(directory)
+    
+    relFiles = [k for k in listFiles if target in k]
+    if relFiles:
+        num = 1
+        while(target + str(num) + ".txt" in relFiles):
+            relFiles.remove(target + str(num) + ".txt")
+            num += 1
+        f = open(directory + "/" + target + str(num) + ".txt", "w+")
+    else:
+        f = open(directory + "/" + target + "1.txt", "w+")
+    return f
+    
+def startTest(test):
+    if test == 1:
+        print("starting up - press the button when the LED turns on")
+        for i in range(3):
+            GPIO.output(LED, GPIO.HIGH)
+            time.sleep(0.25)
+            GPIO.output(LED, GPIO.LOW)
+            time.sleep(0.25)
+    elif test == 2:
+        print("starting up - press the button when the LED turns off")
+        for i in range(3):
+            GPIO.output(LED, GPIO.LOW)
+            time.sleep(0.25)
+            GPIO.output(LED, GPIO.HIGH)
+            time.sleep(0.25)
+    elif test == 3:
+        print("starting up - press the button when the buzzer turns on")
+        for i in range(3):
+            GPIO.output(buzzer, GPIO.HIGH)
+            time.sleep(0.25)
+            GPIO.output(buzzer, GPIO.LOW)
+            time.sleep(0.25)
+    elif test == 4:
+        print("starting up - press the button when the buzzer turns off")
+        for i in range(3):
+            GPIO.output(buzzer, GPIO.LOW)
+            time.sleep(0.25)
+            GPIO.output(buzzer, GPIO.HIGH)
+            time.sleep(0.25)
 
-    global start
-    global end
 
-    time.sleep(10)
-    Blink(3)
+def endTest(test):
+    if test == 1:
+        GPIO.output(LED, GPIO.LOW)
+    elif test == 2:
+        GPIO.output(LED, GPIO.LOW)
+    elif test == 3:
+        GPIO.output(buzzer, GPIO.LOW)
+    elif test == 4:
+        GPIO.output(buzzer, GPIO.LOW)
 
-    while True:
-        turnOn = 5
-        while (turnOn) > 0:
-            if (turnOn) > 0:  # do turnOn
-                GPIO.output(3, False)
-                time.sleep(random.uniform(1, 4))
-                GPIO.add_event_detect(2, GPIO.FALLING, callback, bouncetime=200)
-                GPIO.output(3, True)
-                start = timer()
-                end = start
-                time.sleep(2)
-                GPIO.remove_event_detect(2)
-                diff = end - start
-                order = 6 - turnOn
-                print('No.' + str(order) + ' try, ', diff)
-                Reaction.append(diff)
-                file.write(Gender[x] + '	' + str(Reaction[5*x+(order-1)]) + '\n')
-                turnOn -= 1
-        while True:
-            answer = input('Press c to continue, or press q to quit. ')
-            if (answer[0] == 'q' or answer[0] == 'Q' or answer[0] == 'c' or answer[0] == 'C') : break
-        
-        if (answer[0] == 'q' or answer[0] == 'Q'):
-            file.close()
-            print('All done. File written called ' + file_name + '. ')
-            exit(0)
-        elif (answer[0] == 'c' or answer[0] == 'C'):
-            GPIO.setwarnings(False)
-            GPIO.setmode(GPIO.BCM)
-            GPIO.setup(2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-            GPIO.setup(3, GPIO.OUT)  # LED
-            GPIO.output(3, False)
-            break
-            
+def endWait(test):
+    if test == 1:
+        GPIO.output(LED, GPIO.HIGH)
+    elif test == 2:
+        GPIO.output(LED, GPIO.LOW)
+    elif test == 3:
+        GPIO.output(buzzer, GPIO.HIGH)
+    elif test == 4:
+        GPIO.output(buzzer, GPIO.LOW)
+
+def testReaction(test):
+    startTest(test)
+    time.sleep(random.random() * 2 + 2)
+    if GPIO.input(button):
+        print("cheater... wait until the signal to press the button")
+        print("restarting")
+        endTest(test)
+        test = random.choice(tests)
+        testReaction(test)
+        return
+    endWait(test)
+    start = time.clock()
+    while not GPIO.input(button):
+        pass
+    final = time.clock()
+    GPIO.output(LED, GPIO.LOW)
+    GPIO.output(buzzer, GPIO.LOW)
+    result = final - start
+    print('your response time was {0} seconds!'.format(result))
+    tests.remove(test)
+    if test == 1:
+        test = "LED_ON"
+    elif test == 2:
+        test = "LED_OFF"
+    elif test == 3:
+        test = "BUZZ_ON"
+    elif test == 4:
+        test = "BUZZ_OFF"
+    f.write("%s,%s,%s,%s\n" % (piNum, user, test, result))
+
+user = input("please enter your name: ")
+f = determineFilename(user)
+f.write("board,user,test,time-elapsed\n")
+while len(tests):
+    test = random.choice(tests)
+    testReaction(test)
+print("testing complete")
+GPIO.cleanup()
+exit()
